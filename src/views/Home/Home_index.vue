d503563332c26f978135de617a754d1f22e4c5d4
lim1t0722 <2072174762@qq.com>
2025-01-23 11:38:58 +0800

    home搭建-分类数据渲染

--- src/views/Home/Home_index.vue
+++ src/views/Home/Home_index.vue
@@ -1,3 +1,17 @@
+<script setup>
+import HomeBanner from './components/HomeBanner.vue'
+import HomeCategory from './components/HomeCategory.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeProduct from './components/HomeProduct.vue'
+</script>
+
 <template>
-    <div style="height: 1200px;"></div>
+    <div class="container">
+        <HomeBanner />
+        <HomeCategory />
+    </div>
+        <HomeHot />
+        <HomeNew />
+        <HomeProduct />
 </template>
