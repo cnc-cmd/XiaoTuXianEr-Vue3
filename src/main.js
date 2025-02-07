1c1fae6d478ceb59519edcd9373abfe7dc203ac8
lim1t0722 <2072174762@qq.com>
2025-02-07 09:00:46 +0800

    home-图片懒加载指令

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
    
-
+import { useIntersectionObserver } from '@vueuse/core'
 //引入初始化样式文件
 import './styles/common.scss'
 
@@ -15,3 +15,19 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+//定义全局指令
+app.directive('img-lazy' , {
+    mounted (el, binding) {
+        console.log(el, binding.value)
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+                console.log(isIntersecting)
+                if (isIntersecting) {
+                    el.src = binding.value
+                }
+            },
+        )
+    }
+})
